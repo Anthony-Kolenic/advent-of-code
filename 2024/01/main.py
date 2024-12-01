e7ae6ed10a44c5042526dcb419ae1231d83e454a
Anthony Kolenic <kolenic.anthony@gmail.com>
2024-12-01 09:21:07 +0200

    Code cleanup

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -1,28 +1,22 @@
 from collections import Counter
 
 def main():
-    with open("input.txt") as f:
-        lines = [x.rstrip().replace("   ", " ") for x in f.readlines()]
-    
     list_a: list[int] = []
     list_b: list[int] = []
-    for line in lines:
-        [a,b] = line.split(" ")
-        list_a.append(int(a))
-        list_b.append(int(b))
+
+    with open("input.txt") as f:
+        for line in f.readlines():
+            [a,b] = line.rstrip().split("   ")
+            list_a.append(int(a))
+            list_b.append(int(b))
     
     list_a.sort()
     list_b.sort()
-
-    difference = 0
-    for a,b in zip(list_a, list_b):
-        difference += abs(a - b)
+    difference = sum([abs(a - b) for (a, b) in zip(list_a, list_b)])
     print("Part A:", difference)
 
     counts = Counter(list_b)
-    similarity = 0
-    for a in list_a:
-        similarity += a * counts.get(a, 0)
+    similarity = sum([x * counts.get(x, 0) for x in list_a])
     print("Part B:", similarity)
 
 if __name__ == "__main__":
