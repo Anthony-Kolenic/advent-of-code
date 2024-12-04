30cf83c19b50bdac5db6c32d34b9ecd10b2c3b27
Anthony Kolenic <kolenic.anthony@gmail.com>
2024-12-04 19:27:51 +0200

    Added 2024 Day 04

--- 2024/03/main.py
+++ 2024/03/main.py
@@ -1,10 +1,9 @@
 import re
 
-def process_b(items: list[str]):
+def process(items: list[str]):
     total = 0
     can_process = True
     for item in items:
-        item = next((x for x in item if x))
         match item:
             case "do()":
                 can_process = True
@@ -17,21 +16,14 @@ def process_b(items: list[str]):
                 total += int(a) * int(b)
     return total
 
-def process(items: list[str]):
-    total = 0
-    for item in items:
-        [a,b] = item.replace("mul(", "").replace(",", " ").replace(")", "").split()
-        total += int(a) * int(b)
-    return total
-
 def main():
     data = ""
     with open("input.txt") as f:
         data = "".join([x.rstrip() for x in f.readlines()])
     part_a = r"(mul\(\d+,\d+\))"
-    part_b = r"(mul\(\d+,\d+\))|(do\(\))|(don't\(\))"
+    part_b = r"((?:mul\(\d+,\d+\))|(?:do\(\))|(?:don't\(\)))"
     print("Part A:", process(re.findall(part_a, data)))
-    print("Part B:", process_b(re.findall(part_b, data)))
+    print("Part B:", process(re.findall(part_b, data)))
 
 if __name__ == "__main__":
     main()
---
+++ 2024/04/main.py
@@ -0,0 +1,67 @@
+
+def rotate_matrix(matrix: list[list]):
+    rows, cols = len(matrix), len(matrix[0])
+    result = []
+    for c in range(cols):
+        result.append(["." for _ in range(rows)])
+
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            result[c][r] = matrix[r][c]
+    return result
+    # Intresting way to achieve the same logic 
+    # list(zip(*matrix[::-1]))
+    # reverse matrix row order, unpack, and finally group the items with zip
+
+def flip_matrix(grid):
+    return [x[::-1] for x in grid]
+
+def search_grid(grid) -> int:
+    total = 0
+    for row in grid:
+        line = "".join(row)
+        total += line.count("XMAS")
+        total += line.count("SAMX")
+    return total
+
+def diagonal_search(grid, search = "XMAS") -> list[tuple[int, int]]:
+    total = 0
+    search_rev = search[::-1]
+    result = []
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            try:
+                word = ""
+                for i in range(len(search)):
+                    word += grid[r + i][c + i]
+                if word == search or word == search_rev:
+                    total += 1
+                    result.append((r, c))
+            except:
+                # Except when out of bounds, so we skip these
+                continue    
+    return result
+
+def xmas_scan(grid) -> int:
+    first_scan = diagonal_search(grid, "MAS")
+    second_scan = diagonal_search(flip_matrix(grid), "MAS")
+    # flip the positions back to the orginal matrix coord system
+    second_scan = [(a,len(grid) - 1 - b) for (a,b) in second_scan]
+    total = 0
+    for (a_y, a_x) in first_scan:
+        for (b_y, b_x) in second_scan:
+            if (a_y == b_y) and (b_x - a_x == 2):
+                total += 1
+    return total
+
+def main():
+    grid : list[list[str]] = []
+    with open("input.txt") as f:
+        for line in f.readlines():
+            grid.append([(x if x in "XMAS" else ".") for x in line.rstrip()])
+    result = search_grid(grid) + search_grid(rotate_matrix(grid)) + len(diagonal_search(grid)) + len(diagonal_search(flip_matrix(grid)))
+    print("Part A:", result)
+    print("Part B:", xmas_scan(grid))
+    
+if __name__ == "__main__":
+    main()
